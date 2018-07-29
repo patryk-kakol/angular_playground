339987416aa29d38d43555b67e97f0582e124250
Patryk Kąkol <patryklkakol@gmail.com>
2018-07-29 21:01:08 +0200

    recipe model, display test

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Dummy Recipe 1',
+      'test test test test test',
+      'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d'
+    ),
+    new Recipe(
+      'Dummy Recipe 2',
+      'test test test test test',
+      'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d'
+    )
+  ];
 
   constructor() { }
 
