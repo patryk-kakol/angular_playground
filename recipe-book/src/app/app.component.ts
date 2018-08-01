f66a140cf1f42706a65a1212af77b3d4935802fc
Patryk Kąkol <patryklkakol@gmail.com>
2018-08-02 00:00:01 +0200

    data passing from navbar to features (eventEmitter)

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  selectedFeature = 'recipe';
+
+  onSelection(feature) {
+    this.selectedFeature = feature;
+  }
 }
