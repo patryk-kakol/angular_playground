66cc1d15dbb1581ae87fa064eef0b5b5e3aa5ad0
Patryk Kąkol <patryklkakol@gmail.com>
2018-08-03 23:45:44 +0200

    Revert "set up custom directive"
    This reverts commit db8838bd253996c621cd546c1e3c2705a27a2bd3.

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -20,8 +19,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent,
-    DropdownDirective
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
