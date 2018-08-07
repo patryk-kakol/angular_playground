7c3ec15210cf9036b469429cfe3a65e5e781e4bf
Patryk Kąkol <patryklkakol@gmail.com>
2018-08-08 00:36:15 +0200

    ingredients adding

--- recipe-book/src/app/shopping-list/shopping-list.service.ts
+++ recipe-book/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,9 @@
 import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
+
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Pinapples', 2),
@@ -14,6 +17,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 
 }
