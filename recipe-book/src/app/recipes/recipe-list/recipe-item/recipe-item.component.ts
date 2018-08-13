6b95a375b46b80df7e7ba81053a8121c0a889cd6
Patryk Kąkol <patryklkakol@gmail.com>
2018-08-14 00:09:43 +0200

    child routes

--- recipe-book/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,11 +9,8 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipe: Recipe;
-  onSelected() {
-  this.recipeService.recipeSelected.emit(this.recipe);
-}
 
-  constructor(private recipeService: RecipeService) { }
+
 
   ngOnInit() {
   }
